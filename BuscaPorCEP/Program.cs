be00633c7abecaa96a0e9bd0862e092d783fe579
Lucas Andrade dos Santos <lucas.santos@tbfg.com.br>
2023-11-17 14:37:24 -0300

    Adicionar arquivos de projeto.

---
+++ BuscaPorCEP/Program.cs
@@ -0,0 +1,20 @@
+namespace BuscaPorCEP
+{
+    /// <summary>
+    /// Classe de programa padrão para criação da aplicação.
+    /// As configurações do 'builder' e 'app' estão no appsettings.json, que
+    /// é incorporado no executável da aplicação. Configurações padrão estão
+    /// incluídas nas DLLs das bibliotecas (Hot.DLL e HotAPI.DLL)
+    /// </summary>
+    public class Program : HotAPIServer
+    {
+        /// <summary>
+        /// Método 'main' padrão da HotAPI
+        /// </summary>
+        /// <param name="args"></param>
+        public static void Main(string[] args)
+        {
+            MainDefault<Program>();
+        }
+    }
+}
